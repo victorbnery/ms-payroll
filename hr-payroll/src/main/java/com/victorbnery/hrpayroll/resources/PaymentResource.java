4bbc02334eeb372028751e9f31e336345a263b05
Victor Beckman Nery <victorbnery@gmail.com>
2021-11-24 12:11:14 -0300

    Rest template

--- hr-payroll/src/main/java/com/victorbnery/hrpayroll/resources/PaymentResource.java
+++ hr-payroll/src/main/java/com/victorbnery/hrpayroll/resources/PaymentResource.java
@@ -18,7 +18,7 @@ public class PaymentResource {
 	private PaymentService service;
 	
 	@GetMapping(value = "/{workerId}/days/{days}")
-	public ResponseEntity<Payment> getPayment (@PathVariable long workerId, @PathVariable Integer days){
+	public ResponseEntity<Payment> getPayment (@PathVariable Long workerId, @PathVariable Integer days){
 		Payment payment = service.getPayment(workerId, days);
 		return ResponseEntity.ok(payment);
 	}
